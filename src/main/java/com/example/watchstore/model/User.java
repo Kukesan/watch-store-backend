f9a8fa9d4ca85fd15d6b25d3da47f2c3037060ab
Kukesan <kkukesan@gmail.com>
2024-03-14 14:21:56 +0530

    contact api created

--- src/main/java/com/example/watchstore/model/User.java
+++ src/main/java/com/example/watchstore/model/User.java
@@ -1,18 +1,18 @@
-package com.example.watchstore.model;
-
-import jakarta.persistence.*;
-@Entity
-@Table(name = "users")
-public class User implements UserDetails {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String email;
-    // Other fields and relationships
-    // Implement UserDetails methods and other required methods
-    // Getters and setters
-}
+//package com.example.watchstore.model;
+//
+//import jakarta.persistence.*;
+//@Entity
+//@Table(name = "users")
+//public class User implements UserDetails {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    @Column(unique = true)
+//    private String username;
+//    private String password;
+//    private String email;
+//    // Other fields and relationships
+//    // Implement UserDetails methods and other required methods
+//    // Getters and setters
+//}
